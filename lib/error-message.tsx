11e92f74aa381da47f200892df14bf28c2beff90
Ali Raza <alirazakhalid1129@gmail.com>
2026-04-18 11:17:50 +0500

    (refactor): enhance errorMessage function to include optional message parameter

--- lib/error-message.tsx
+++ lib/error-message.tsx
@@ -1,16 +1,20 @@
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 import { APIError } from "better-auth/api";
 
-export function errorMessage(error: unknown): string {
+export function errorMessage(error: unknown, message?: string): string {
+  const suffix = message ? ` ${message}` : "";
+
   if (error instanceof APIError) {
-    return ` ${error.status} - ${error.body?.message}`;
-  } else if (
-    error instanceof PrismaClientKnownRequestError &&
-    error.code === "P2025"
-  ) {
+    return `${error.status} - ${error.body?.message}${suffix}`;
+  } else if (error instanceof PrismaClientKnownRequestError) {
+    if (error.code === "P2025") {
+      return `Resource not found: ${suffix}`;
+    }
+    if (error.code === "P2002") {
+      return `Resource already exists: ${suffix}`;
+    }
     console.log(error.name);
-    return "Resource not found";
   }
 
-  return "Something went wrong!";
+  return `Something went wrong: ${suffix}`;
 }
